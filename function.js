f62598cd64bd97949f93e52f6e11679c2845179f
giovannyrosado <grosado@racc.edu>
2018-02-09 02:37:07 -0500

    added Code Challenge 3

--- function.js
+++ function.js
@@ -1,19 +1,7 @@
-var name = windows.prompt("Please enter your name:")
-var ageCheck = window.prompt (Please enter your age)
+var number = 1;
 
-if (ageCheck < 21) {
+  while (number <= 5) {
+  console.log(number);
 
-      window.alert("You submited that you are :" + ageCheck +": years old, " + name + ".");
-      window.alert("You must be at least 21 years old to drink in the state of PA");
-}
-
-    else if (ageCheck > 21 && ageCheck < 101) {
-
-      window.alert("You are old enough to drink in PA");
-      window.alert("Welcome " + name + "!");
-    }
-
-else {
-      window.alert("You best not be lying about your age, " + name + ".");
-
+  number++;
 }
